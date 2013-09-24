abe9a2fa458b68a196b5864d45962813d590a192
Andrea Biancini <andrea.biancini@gmail.com>
2013-09-24 16:14:39 +0200

    Insert in mysql table

--- downloader.py
+++ downloader.py
@@ -1,22 +1,47 @@
+#!/usr/bin/python
+
 import twitter
+import MySQLdb as mdb
+from datetime import datetime
 from secrets import consumer_key, consumer_secret, access_token_key, access_token_secret
+from secrets import dbhost, dbuser, dbpass, dbname
 
 api = twitter.Api(consumer_key = consumer_key,
                   consumer_secret = consumer_secret,
                   access_token_key = access_token_key,
                   access_token_secret = access_token_secret)
 
-#print api.VerifyCredentials()
-
-#statuses = api.GetUserTimeline('andreabiancini')
-#statuses = api.GetSearch('renzi :)') #UserTimeline('andreabiancini')
-
+lat = 45.776665
+lng = 3.07723
+radius = '400km'
+count = 100
+ripetizioni = 1
 lastid = None
-for i in [1, 2, 3, 4]:
-	statuses = api.GetSearch(geocode=(41.892916, 12.482520, '500km'), count=100, max_id=lastid) #UserTimeline('andreabiancini')
-	lastid = min([s.id for s in statuses])
-	printouts = [s.text for s in statuses]
-	for curprint in printouts:
-		print curprint
+lang = 'fr'
 
+from secrets import dbhost, dbuser, dbpass, dbname
+con = mdb.connect(dbhost, dbuser, dbpass, dbname)
+with con:
+	cur = con.cursor()
 
+	for i in range(0, ripetizioni):
+		statuses = api.GetSearch(geocode=(lat, lng, radius), count=count, max_id=lastid, lang=lang)
+	        #statuses = api.GetUserTimeline('andreabiancini')
+		lastid = min([s.id for s in statuses])
+
+		print "Number of tweets downloaded:\t%d\n" % len(statuses)
+		for s in statuses:
+			date_object = datetime.strptime(s.created_at, '%a %b %d %H:%M:%S +0000 %Y')
+			sql = "INSERT INTO tweets (`tweetid`, `timestamp`, `text`, `hashtags`) VALUES ('%s', '%s', '%s', '%s')" % (s.id, date_object.strftime('%Y-%m-%d %H:%M:%S'), s.text.replace('\'', '\\\''), s.hashtags)
+			try:
+				cur.execute(sql)
+				con.commit()
+			except Exception, e:
+				print sql
+				print "Exception: %s" % e
+				con.rollback()
+
+		#printouts = [s.text for s in statuses]
+		#for curprint in printouts: print "[%s]" % curprint
+
+print "Finish"
