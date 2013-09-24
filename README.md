75569661862911dae1c0f86ea81c370711e08526
Andrea Biancini <andrea.biancini@gmail.com>
2013-09-24 08:30:41 +0200

    Reviewed prerequisites

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ PythonDownloader
 ================
 
 Python tests to interact with Twitter API
+
+To install prerequisites use the command:
+```
+sudo pip install -r requirements.txt
+```
--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 import twitter
 from secrets import consumer_key, consumer_secret, access_token_key, access_token_secret
 
-api = twitter.Api(consumer_key = consumer_key,,
+api = twitter.Api(consumer_key = consumer_key,
                   consumer_secret = consumer_secret,
                   access_token_key = access_token_key,
                   access_token_secret = access_token_secret)
