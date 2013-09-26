be0aa09363b881610b276746049c71b32b832f26
biancini <andrea.biancini@gmail.com>
2013-09-26 23:50:42 +0200

    Added streaming API

---
+++ downloader_stream.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from TwitterAPI import TwitterAPI
+import MySQLdb
+from datetime import datetime
+from secrets import consumer_key, consumer_secret, auth_type, access_token_key, access_token_secret
+from secrets import dbhost, dbuser, dbpass, dbname
+
+class TwitterApiCall(object):
+  api = TwitterApi(consumer_key = consumer_key,
+                   consumer_secret = consumer_secret,
+                   access_token_key = access_token_key,
+                   access_token_secret = access_token_secret)
+
+class DownloadFrenchTweets(TwitterApiCall):
+  con = None
+
+  def __init__(self):
+    try:
+      self.con = MySQLdb.connect(host = dbhost,
+                                 user = dbuser,
+                                 passwd = dbpass,
+                                 db = dbname,
+                                 charset = 'utf8')
+      print "Connected to MySQL db %s:%s." % (dbhost, dbname)
+    except Exception, e:
+      print "Error connecting to MySQL db %s:%s: %s" % (dbhost, dbname, e)
+
+  def __del__(self):
+    if self.con: self.con.close()
+
+  def SelectMaxTweetId(self):
+    cur = self.con.cursor()
+    try:
+      cur.execute("SELECT MAX(`tweetid`) FROM tweets")
+      firstid = cur.fetchone()[0]
+      print "The last tweetid in table is %s." % firstid
+    except Exception, e:
+      firstid = None
+    return firstid
+
+  def InsertTweetsIntoDb(self):
+    print 'Executing Twitter API calls '
+
+    cur = self.con.cursor()
+
+    r = self.api.request('statuses/filter', {'locations':'-5.141602,43.612217,7.77832,43.707594'})
+    for item in r.get_iterator():
+      print item['text']
+      date_object = datetime.strptime(item['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
+      text = item['text'].encode(encoding='ascii', errors='ignore').decode(encoding='ascii', errors='ignore')
+
+      sql_vals = (item['id'],
+                  date_object.strftime('%Y-%m-%d %H:%M:%S'),
+                  text.replace('\'', '\\\''),
+                  ', '.join([h.text for h in s.hashtags]))
+
+      sql  = 'INSERT INTO tweets (`tweetid`, `timestamp`, `text`, `hashtags`)'
+      sql += 'VALUES (\'%s\', \'%s\', \'%s\', \'%s\')' % sql_vals
+
+      try:
+        cur.execute(sql)
+        self.con.commit()
+      except Exception as e:
+        self.con.rollback()
+
+if __name__ == "__main__":
+  engine = DownloadFrenchTweets()
+  #engine.SelectMaxTweetId()
+  #print engine.GetCurrentLimit()
+  #print engine.PrintRateLimit()
+  engine.InsertTweetsIntoDb()
