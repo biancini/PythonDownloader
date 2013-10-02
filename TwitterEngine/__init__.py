a5c53970bf61eeefbfc01e4f6b214e786888841c
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-02 16:31:19 +0200

    Reviewed

--- TwitterEngine/__init__.py
+++ TwitterEngine/__init__.py
@@ -8,8 +8,12 @@ __date__ = "October 2, 2013"
 from .rest import DownloadTweetsREST
 from .stream import DownloadTweetsStream
 
+from .backend import BackendError
+from .mysqlbackend import MySQLBackend
 
 __all__ = [
 	'DownloadTweetsREST', 
-	'DownloadTweetsStream'
+	'DownloadTweetsStream',
+        'BackendError',
+        'MySQLBackend'
 ]
