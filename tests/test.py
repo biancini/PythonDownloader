9604939a0b9938e64aa4fdc4569511a61cf8c203
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-03 12:28:43 +0200

    added tests

---
+++ tests/test.py
@@ -0,0 +1,24 @@
+# create an empty image with PIL and put pixels inside
+from PIL import Image
+
+# use a (r, g, b) tuple to represent colors
+red = (255,0,0)
+#white = (255,255,255)
+
+# create a new 256x256 pixel image surface
+# make the background white (default bg=black)
+#img = Image.new("RGB", [256,256], white)
+img = Image.open("france.png")
+
+x = 50
+y = 40
+# put a red pixel at point (x, y)
+img.putpixel((x, y), red)
+img.putpixel((x+1, y), red)
+img.putpixel((x, y+1), red)
+img.putpixel((x+1, y+1), red)
+
+# save the image
+img.save("test1.png")
+# optionally look at the image you have created
+img.show()
