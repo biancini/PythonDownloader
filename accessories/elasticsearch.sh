b8696c124746b6d57cdb3feab4efb03e77d8ead5
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-09 16:54:59 +0200

    Reviewed analyzers

--- accessories/elasticsearch.sh
+++ accessories/elasticsearch.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 #"EEE M dd HH:mm:ss +0000 yyyy"
-curl -XPOST 'http://localhost:9200/twitter/tweets/_mapping' -d '{ "tweets" : { "properties" : { "created_at": { "type": "date", "format" : "yyyy-MM-dd HH:mm:ss" }, "location" : { "type": "string", "index": "not_analyzed" }, "coordinates": { "type": "geo_point" } } } }'
+curl -XPOST 'http://localhost:9200/twitter/tweets/_mapping' -d '{ "tweets" : {
+	"properties" : {
+		"created_at": { "type": "date", "format" : "yyyy-MM-dd HH:mm:ss" },
+		"location" : { "type": "string", "index": "not_analyzed" },
+		"coordinates": { "type": "geo_point" },
+                "text": { "type": "string", "analyzer": "french" },
+                "hashtags": { "type": "string" }
+	}
+} }' 
 
+if [ "$?" -eq "0" ];
+then
 curl -XGET 'http://localhost:9200/twitter/tweets/_mapping?pretty=true'
+fi
+
