fcaec07cb6944e2b9f1bb2508d71e861fc2f755d
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-16 08:34:11 +0200

    Added lock to avoid multiple executions

--- engine.py
+++ engine.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+
 from TwitterEngine import DownloadTweetsREST, DownloadTweetsStream
 
 if __name__ == "__main__":
@@ -12,9 +14,20 @@ if __name__ == "__main__":
     print 'Wrong mechanism passed, assuming rest.'
     mechanism = 'rest'
 
-  if mechanism == 'rest':
-    engine = DownloadTweetsREST('oAuth2')
+  lock_file = '/var/lock/twitter.lock'
+  if os.path.exists(lock_file):
+    print "Stopping because another process is already running."
+    print "This script is locked with %s" % lock_file
+    sys.exit(-1)
   else:
-    engine = DownloadTweetsStream('oAuth1')
+    open(lock_file, "w").write("1")
+    try:
+      if mechanism == 'rest': engine = DownloadTweetsREST('oAuth2')
+      else: engine = DownloadTweetsStream('oAuth1')
 
-  engine.ProcessTweets()
+      engine.ProcessTweets()
+    except Exception as e:
+      print "Received exception: %s" % e
+    finally:
+      os.remove(lock_file)
+
