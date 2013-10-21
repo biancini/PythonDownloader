0581dd3cb57192dfd8cb8d9de6b455f1266debc3
Andrea Biancini <andrea.biancini@gmail.com>
2013-10-21 14:51:40 +0200

    Added date choosing mechanism to main

--- JavaAnalyzer/src/fr/twitteranalyzer/SampleAnalyzer.java
+++ JavaAnalyzer/src/fr/twitteranalyzer/SampleAnalyzer.java
@@ -1,5 +1,7 @@
 package fr.twitteranalyzer;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -10,11 +12,16 @@ public class SampleAnalyzer {
 
 	/**
 	 * @param args
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		try {
 			ByPersonAnalyzer analyzer = new ByPersonAnalyzer();
+			
 			Date filterDate = Calendar.getInstance().getTime();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			filterDate = sdf.parse("2013-10-18");
+			
 			analyzer.runAnalysis(filterDate, filterDate);
 		} catch (AnalyzerException e) {
 			e.printStackTrace();
