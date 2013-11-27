32b5d2744cb39ad0de516fe6bf4c5f03885066a0
Andrea Biancini <andrea.biancini@gmail.com>
2013-11-27 12:27:54 +0100

    Refactored runner

--- JavaAnalyzer/src/fr/twitteranalyzer/Runner.java
+++ JavaAnalyzer/src/fr/twitteranalyzer/Runner.java
@@ -1,12 +1,12 @@
 package fr.twitteranalyzer;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import fr.twitteranalyzer.elastic.ByPersonAnalyzer;
 import fr.twitteranalyzer.exceptions.AnalyzerException;
+import fr.twitteranalyzer.utils.DateUtils;
 
 //import fr.twitteranalyzer.mapreduce.ByPersonAnalyzer;
 
@@ -18,15 +18,14 @@ public class Runner {
 	 */
 	public static void main(String[] args) throws ParseException {
 		try {
+			String dayOfInterest = "2013-11-27";
 			ByPersonAnalyzer analyzer = new ByPersonAnalyzer();
 
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-
 			Date fromDate = Calendar.getInstance().getTime();
-			fromDate = sdf.parse("2013-11-26");
+			fromDate = DateUtils.parseDate(dayOfInterest);
 
 			Date toDate = Calendar.getInstance().getTime();
-			toDate = sdf.parse("2014-01-26");
+			toDate = DateUtils.parseDate(dayOfInterest);
 
 			analyzer.runAnalysis(fromDate, toDate);
 			System.out.println("Finished.");
