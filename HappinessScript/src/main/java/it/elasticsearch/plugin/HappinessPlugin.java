df8a4c80610bfe798af12bbafc78755dd4c74546
Andrea Biancini <andrea.biancini@gmail.com>
2013-12-30 11:56:10 +0100

    Created ES plugin structure

--- HappinessScript/src/main/java/it/elasticsearch/plugin/HappinessPlugin.java
+++ HappinessScript/src/main/java/it/elasticsearch/plugin/HappinessPlugin.java
@@ -7,24 +7,11 @@ import org.elasticsearch.script.ScriptModule;
 
 public class HappinessPlugin extends AbstractPlugin {
 
-	/**
-	 * The name of the plugin.
-	 * <p/>
-	 * This name will be used by elasticsearch in the log file to refer to this
-	 * plugin.
-	 * 
-	 * @return plugin name.
-	 */
 	@Override
 	public String name() {
 		return "happiness-script";
 	}
 
-	/**
-	 * The description of the plugin.
-	 * 
-	 * @return plugin description
-	 */
 	@Override
 	public String description() {
 		return "Happiness script";
