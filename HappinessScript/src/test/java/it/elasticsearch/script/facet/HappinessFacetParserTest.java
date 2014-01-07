10d9b3e0376751ec6ff96aa2691f599473079c5e
Andrea Biancini <andrea.biancini@gmail.com>
2014-01-07 18:23:37 +0100

    Addded some test

--- HappinessScript/src/test/java/it/elasticsearch/script/facet/HappinessFacetParserTest.java
+++ HappinessScript/src/test/java/it/elasticsearch/script/facet/HappinessFacetParserTest.java
@@ -10,6 +10,7 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.search.facet.FacetExecutor;
+import org.elasticsearch.search.facet.FacetPhaseExecutionException;
 import org.elasticsearch.search.internal.SearchContext;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -18,13 +19,45 @@ public class HappinessFacetParserTest {
 
 	public static final String FACET_NAME = "facetname";
 
+	@Test(expected = FacetPhaseExecutionException.class)
+	public void shouldParseComplainIfNoMapScript() throws IOException {
+		// given
+		Settings settings = Mockito.mock(Settings.class);
+		Mockito.when(settings.get(Mockito.anyString())).thenReturn("");
+		Mockito.when(settings.getAsBoolean(Mockito.anyString(), Mockito.anyBoolean())).thenReturn(false);
+		HappinessFacetParser happinessFacetParser = new HappinessFacetParser(settings, null, null);
+
+		Map<String, Object> expectedCombineScript = new HashMap<String, Object>();
+		expectedCombineScript.put("script", "happycombiner");
+		expectedCombineScript.put("lang", "native");
+
+		Map<String, Object> expectedReduceScript = new HashMap<String, Object>();
+		expectedReduceScript.put("script", "happyreducer");
+		expectedReduceScript.put("lang", "native");
+
+		String jsonData = "{ ";
+		jsonData += "\"combine_script\": { \"script\": \"" + expectedCombineScript.get("script")
+				+ "\", \"lang\": \"" + expectedCombineScript.get("lang") + "\" }, ";
+		jsonData += "\"reduce_script\": { \"script\": \"" + expectedReduceScript.get("script")
+				+ "\", \"lang\": \"" + expectedReduceScript.get("lang") + "\" } ";
+		jsonData += "}";
+
+		XContentParser parser = JsonXContent.jsonXContent.createParser(jsonData);
+		SearchContext context = null;
+
+		// when
+		happinessFacetParser.parse(FACET_NAME, parser, context);
+
+		// then
+	}
+
 	@Test
 	public void shouldParseProcessAllInput() throws IOException {
 		// given
 		Settings settings = Mockito.mock(Settings.class);
 		Mockito.when(settings.get(Mockito.anyString())).thenReturn("");
 		Mockito.when(settings.getAsBoolean(Mockito.anyString(), Mockito.anyBoolean())).thenReturn(false);
-		HappinessFacetParser happinessFacetCollector = new HappinessFacetParser(settings, null, null);
+		HappinessFacetParser happinessFacetParser = new HappinessFacetParser(settings, null, null);
 
 		Map<String, Object> expectedCombineScript = new HashMap<String, Object>();
 		expectedCombineScript.put("script", "happycombiner");
@@ -41,19 +74,64 @@ public class HappinessFacetParserTest {
 		jsonData += "\"reduce_script\": { \"script\": \"" + expectedReduceScript.get("script")
 				+ "\", \"lang\": \"" + expectedReduceScript.get("lang") + "\" } ";
 		jsonData += "}";
-		System.err.println(jsonData);
+
 		XContentParser parser = JsonXContent.jsonXContent.createParser(jsonData);
 		SearchContext context = null;
 
 		// when
-		FacetExecutor facetExecutor = happinessFacetCollector.parse(FACET_NAME, parser, context);
+		FacetExecutor facetExecutor = happinessFacetParser.parse(FACET_NAME, parser, context);
 
 		// then
-		assertThat(happinessFacetCollector).isInstanceOf(HappinessFacetParser.class);
+		assertThat(happinessFacetParser).isInstanceOf(HappinessFacetParser.class);
 		HappinessFacetExecutor hapinessFacetExecutor = (HappinessFacetExecutor) facetExecutor;
 		assertThat(hapinessFacetExecutor.getCombineScript().entrySet()).containsOnly(
 				expectedCombineScript.entrySet().toArray());
 		assertThat(hapinessFacetExecutor.getReduceScript().entrySet()).containsOnly(
 				expectedReduceScript.entrySet().toArray());
+	}
+
+	@Test
+	public void shouldTypesReturnHappinessFacetType() throws IOException {
+		// given
+		Settings settings = Mockito.mock(Settings.class);
+		Mockito.when(settings.get(Mockito.anyString())).thenReturn("");
+		Mockito.when(settings.getAsBoolean(Mockito.anyString(), Mockito.anyBoolean())).thenReturn(false);
+		HappinessFacetParser happinessFacetParser = new HappinessFacetParser(settings, null, null);
+
+		// when
+		String[] returnedTypes = happinessFacetParser.types();
+
+		// then
+		assertThat(returnedTypes).contains(HappinessFacet.TYPE);
+	}
+
+	@Test
+	public void shouldDefaultMainModeReturnCollector() throws IOException {
+		// given
+		Settings settings = Mockito.mock(Settings.class);
+		Mockito.when(settings.get(Mockito.anyString())).thenReturn("");
+		Mockito.when(settings.getAsBoolean(Mockito.anyString(), Mockito.anyBoolean())).thenReturn(false);
+		HappinessFacetParser happinessFacetParser = new HappinessFacetParser(settings, null, null);
+
+		// when
+		FacetExecutor.Mode mode = happinessFacetParser.defaultMainMode();
+
+		// then
+		assertThat(mode).isEqualTo(FacetExecutor.Mode.COLLECTOR);
+	}
+
+	@Test
+	public void shouldDefaultGlobalModeReturnCollector() throws IOException {
+		// given
+		Settings settings = Mockito.mock(Settings.class);
+		Mockito.when(settings.get(Mockito.anyString())).thenReturn("");
+		Mockito.when(settings.getAsBoolean(Mockito.anyString(), Mockito.anyBoolean())).thenReturn(false);
+		HappinessFacetParser happinessFacetParser = new HappinessFacetParser(settings, null, null);
+
+		// when
+		FacetExecutor.Mode mode = happinessFacetParser.defaultGlobalMode();
+
+		// then
+		assertThat(mode).isEqualTo(FacetExecutor.Mode.COLLECTOR);
 	}
 }
