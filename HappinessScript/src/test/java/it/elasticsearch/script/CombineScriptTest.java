9587a20d83a23f7c695b134979cbe480e4cbfaad
Andrea Biancini <andrea.biancini@gmail.com>
2014-01-12 07:51:13 +0100

    Added tests

--- HappinessScript/src/test/java/it/elasticsearch/script/CombineScriptTest.java
+++ HappinessScript/src/test/java/it/elasticsearch/script/CombineScriptTest.java
@@ -67,6 +67,33 @@ public class CombineScriptTest {
 	}
 
 	@Test
+	public void shouldNewScriptReturnWhenParamsIsNotNullAndMultipleElems() throws IOException {
+		// given
+		int numelems1 = 2;
+		int numelems2 = 3;
+		List<ComputedHappiness> listFacets = new ArrayList<ComputedHappiness>();
+		listFacets.add(new ComputedHappiness(score1, relevance1, numelems1));
+		listFacets.add(new ComputedHappiness(score2, relevance2, numelems2));
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put(HappinessInternalFacet.FACET_TYPE, listFacets);
+		CombineScript combineScript = new CombineScript(params);
+
+		int numelems = numelems1 + numelems2;
+		double score = (score1 * numelems1 + score2 * numelems2) / numelems;
+		double relevance = (relevance1 * numelems1 + relevance2 * numelems2) / numelems;
+		List<ComputedHappiness> expectedResult = new ArrayList<ComputedHappiness>();
+		expectedResult.add(new ComputedHappiness(score, relevance, numelems));
+
+		// when
+		Object combineResult = combineScript.run();
+
+		// then
+		assertThat(combineResult).isNotNull();
+		assertThat(combineResult).isEqualTo(expectedResult);
+	}
+
+	@Test
 	public void shouldUnwrapWorkForGoodInput() throws IOException {
 		// given
 		ComputedHappiness happiness1 = new ComputedHappiness(score1, relevance1);
